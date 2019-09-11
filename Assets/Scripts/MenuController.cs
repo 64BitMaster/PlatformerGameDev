566913600012f53ab53189bf865b5b1655531ff4
Marc Sustaita <marc.sustaita@gmail.com>
2019-09-11 13:45:46 -0500

    Initial commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController:MonoBehaviour {
+
+	//	Moves to the next Scene in the Build Order
+    public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	
+    public void ExitGame() {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController:PhysicsObject {
+
+	GameObject Player;
+	Vector3 StartingPositon;
+
+	public float maxSpeed = 7;
+	public float jumpTakeOffSpeed = 7;
+
+	void Start() {
+		Player = GameObject.Find("Player");
+		StartingPositon = Player.transform.position;
+	}
+
+	protected override void ComputeVelocity() {
+		Vector2 move = Vector2.zero;
+
+		move.x = Input.GetAxis("Horizontal");
+
+		if (Input.GetButtonDown("Jump") && grounded) {
+			velocity.y = jumpTakeOffSpeed;
+		} else if (Input.GetButtonUp("Jump")) {
+			if (velocity.y > 0) {
+				velocity.y = velocity.y * 0.5f;
+			}
+		}
+
+		targetVelocity = move * maxSpeed;
+
+		if (Player.transform.position.y < -30F) {
+
+			Player.transform.position = StartingPositon;
+
+		}
+	}
+}
