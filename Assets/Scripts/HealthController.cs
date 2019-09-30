5d1ce5e12054a272fceba9213d9bc8a7ac372734
Marc Sustaita <marc.sustaita@gmail.com>
2019-09-29 21:25:21 -0500

    Added Health Script
    Should be easy to throw on any other enemies later on! To cause damage to something, we should only have to check if a collision occurs, and then call applyDamage() with the amount to take. We might need to adjust things later on to add an invulnerability period.

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+	public int maxHealth = 100;
+	int currentHealth;
+	//public bool damaged = false;
+
+	public Text healthUI;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//adjustDamageIndicator();
+		healthUI.text = currentHealth.ToString();
+    }
+
+
+
+	void applyDamage(int damageTaken) {
+
+		if (damageTaken < currentHealth) {
+			currentHealth -= damageTaken;
+		}
+		else {
+			// Death, idk how we want to handle player death, instant respawn? Or just a normal game over screen?
+		}
+
+		//damaged = true;
+	}
+
+	void applyHeal(int healAmount) {
+
+		currentHealth += healAmount;
+
+		// Cap heal amount to maximum health
+		if (currentHealth > maxHealth) {
+			currentHealth = maxHealth;
+		}
+
+	}
+
+	void adjustHUD() {
+
+	}
+
+	void adjustDamageIndicator() {
+
+		GameObject player = GameObject.Find("Player");
+		Image playerSprite = player.GetComponent<Image>();
+
+		//Color FullyOpaque = new Color(1, 1, 1, 1);
+		//Color FullyTransparent = new Color(1, 1, 1, 0);
+
+		playerSprite.color = Color.Lerp(playerSprite.color, new Color(1, 1, 1, Mathf.PingPong(Time.time, 3f)), 10*Time.deltaTime);
+
+
+	}
+
+
+}
