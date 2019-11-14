94b5c334e1b8a11142ce7167af8a9c38baf8a5bd
64BitMaster <marc.sustaita@gmail.com>
2019-11-13 19:21:05 -0600

    Added Scene Transistions
    Scenes start out black and fade in over a second, and face out when switching over a second as well

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+
+
+    public void FadeToNextLevel()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void FadeAnimationComplete()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+}
