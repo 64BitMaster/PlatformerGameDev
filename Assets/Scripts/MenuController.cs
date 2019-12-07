b426c0ac5e6ecb63df783844a34d18a72d2c9681
64BitMaster <marc.sustaita@gmail.com>
2019-12-07 02:07:18 -0600

    Pause Menu!

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController:MonoBehaviour {
 
-	//	Moves to the next Scene in the Build Order
+
+    //	Moves to the next Scene in the Build Order
     public void StartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-	
+
     public void ExitGame() {
         Debug.Log("Exited");
         Application.Quit();
---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController:MonoBehaviour {
+
+    public static bool gamePaused = false;
+    public GameObject pauseMenuCanvas;
+    public GameObject playerHUD;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(gamePaused == true) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuCanvas.SetActive(false);
+        playerHUD.SetActive(true);
+        Time.timeScale = 1F;
+        gamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Pause() {
+        pauseMenuCanvas.SetActive(true);
+        playerHUD.SetActive(false);
+        Time.timeScale = 0;
+        gamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ReturnToMenuScene() {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
