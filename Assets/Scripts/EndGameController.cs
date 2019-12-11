b47bef967e6db9ee1e37272c9558f778e0b81a35
64BitMaster <marc.sustaita@gmail.com>
2019-12-10 23:46:24 -0600

    Added ending scene, organized builder, and fixed bugs

---
+++ Assets/Scripts/EndGameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameController : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        animator.SetTrigger("FadeOut");
+        Debug.Log("ENDING TRIGGERED!");
+
+    }
+
+}
